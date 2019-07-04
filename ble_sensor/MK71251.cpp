815e88b284c3f89f687a1e3ef158b56e8da87684
Wataru KUNINO <xbee@dream.jp>
2019-07-05 07:24:52 +0900

    Update MK71251.cpp

--- ble_sensor/MK71251.cpp
+++ ble_sensor/MK71251.cpp
@@ -161,7 +161,7 @@ int MK71251::disconnect(){
 
 void MK71251::writeByte(unsigned char in){
 	for(int i=1; i >= 0; i--){
-		byte v = (in >> (8 * i)) & 0x0F;
+		byte v = (in >> (4 * i)) & 0x0F;
 		if(v < 10) v += (byte)'0';
 		else v += (byte)'a' - 10;
 		Serial2.write(v);
